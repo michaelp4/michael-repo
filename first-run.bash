47f0d28e06ed5c7d32f10470a5217977a7b7d895
liorweitz <liorweitz@gmail.com>
2019-03-26 18:58:18 +0200

    first commit

---
+++ first-run.bash
@@ -0,0 +1,13 @@
+#! /bin/bash
+#$ -V
+#$ -cwd
+#$ -N AthTest.sh
+#$ -pe rmpi 4
+#$ -j y
+# -e test.e
+# -o test.o
+
+export MPIRUN=/gpfs0/system/openmpi-2.0.1_openib/bin/mpirun
+machinefile=$TMPDIR/nodes
+awk '{ for (i=0;i<$2;++i) {print $1} }' $PE_HOSTFILE >> $machinefile
+$MPIRUN --hostfile $machinefile -np $NSLOTS /ATHENA DIRECTORY/bin/athena -i <INPUT FILE> &>MyOutputFile
