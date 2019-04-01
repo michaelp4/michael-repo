9195e5e56a08124acf3c1298b27d0c1df41ac5a7
liorweitz <liorweitz@gmail.com>
2019-04-01 16:11:58 +0300

    some

--- first-run.bash
+++ first-run.bash
@@ -10,4 +10,4 @@
 export MPIRUN=/gpfs0/system/openmpi-2.0.1_openib/bin/mpirun
 machinefile=$TMPDIR/nodes
 awk '{ for (i=0;i<$2;++i) {print $1} }' $PE_HOSTFILE >> $machinefile
-$MPIRUN --hostfile $machinefile -np $NSLOTS ../athena-public-version-master/bin/athena -i ../athena-public-version-master/inputs/hydro/athinput.blast &>MyOutputFile
+$MPIRUN --hostfile $machinefile -np $NSLOTS ../athena-public-version-master/bin/athena -i ../athena-public-version-master/inputs/hydro/athinput.blast &>MyOutputFilep
