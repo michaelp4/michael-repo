3c02368da7a21d3a789e97b759c53dba8e4fb455
Michael Pukshanski <mm19924@gmail.com>
2019-08-08 16:14:33 +0300

    adding the blast outputs

--- first-run.bash
+++ first-run.bash
@@ -1,13 +1,15 @@
-#! /bin/bash
+#!/bin/bash
 #$ -V
 #$ -cwd
 #$ -N AthTest.sh
-#$ -pe rmpi 4
+#$ -pe rmpi 1
 #$ -j y
 # -e test.e
 # -o test.o
 
 export MPIRUN=/gpfs0/system/openmpi-2.0.1_openib/bin/mpirun
+
 machinefile=$TMPDIR/nodes
 awk '{ for (i=0;i<$2;++i) {print $1} }' $PE_HOSTFILE >> $machinefile
-$MPIRUN --hostfile $machinefile -np $NSLOTS ../athena-public-version-master/bin/athena -i ../athena-public-version-master/inputs/hydro/athinput.blast &>MyOutputFilep
+
+$MPIRUN --hostfile $machinefile -np $NSLOTS /gpfs0/ukeshet/users/pukshans/athena-public-version-master/bin/athena -i /gpfs0/ukeshet/users/pukshans/athena-public-version-master/inputs/hydro/athinput.blast &>MyOutputFile
